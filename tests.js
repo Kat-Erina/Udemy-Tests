9ea856b35078b97ab138796121845ac9f53c4f17
Kat-Erina <104464629+Kat-Erina@users.noreply.github.com>
2022-07-17 15:57:23 +0400

    Update tests.js

--- tests.js
+++ tests.js
@@ -1,32 +1,3 @@
-// const big=document.querySelector('.big');
-// const little=document.querySelector('.little');
-// const middle=document.querySelector('.middle');
-
-// little.addEventListener('click', (e)=>{
-//     e.target.style.textDecoration='line-through';
-//     e.stopPropagation();
-//     console.log(e.target);
-// });
-
-
-// little.addEventListener('click', (e)=>{
-//     e.target.style.border='1px solid black';
-// });
-// big.addEventListener('click', (e)=>{
-//     e.target.style.fontSize='20px';
-//     console.log(e.target);
-// }, {capture: true})
-
-// middle.addEventListener('click', (e)=>{
-//     e.stopPropagation();
-// })
-
-// document.addEventListener('click', e=>{
-//     console.log(e.target);
-// })
-
-//Popup code//
-
 const form=document.querySelector('form');
 const submit=document.querySelector('submit');
 const pattern=/^[a-zA-Z]{6,12}$/;
